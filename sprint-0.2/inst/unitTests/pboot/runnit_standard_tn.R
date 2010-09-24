e3b642427020b4b047713a53b20c3b9734bccc88
laurence.baldwin <laurence.baldwin@fdf4ec91-bb51-4242-d527-ecd07a88240c>
2010-09-24 15:38:36 +0000

    initial unit tests

---
+++ sprint-0.2/inst/unitTests/pboot/runnit_standard_tn.R
@@ -0,0 +1,46 @@
+##########################################################################
+#                                                                        #
+#  SPRINT: Simple Parallel R INTerface                                   #
+#  Copyright © 2008,2009 The University of Edinburgh                     #
+#                                                                        #
+#  This program is free software: you can redistribute it and/or modify  #
+#  it under the terms of the GNU General Public License as published by  #
+#  the Free Software Foundation, either version 3 of the License, or     #
+#  any later version.                                                    #
+#                                                                        #
+#  This program is distributed in the hope that it will be useful,       #
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the          #
+#  GNU General Public License for more details.                          #
+#                                                                        #
+#  You should have received a copy of the GNU General Public License     #
+#  along with this program. If not, see <http://www.gnu.or/licenses/>.   #
+#                                                                        #
+##########################################################################
+
+
+# = =============================================================== =
+# =  Massive unit test to check all possible combinations of input  =
+# =  parameters and make sure that the output matches the output    =
+# =  from the serial version.                                       =
+# = =============================================================== =
+
+myfunction <- function (data,indices){
+    d <- data[indices]
+    result <- mean(d)
+    return(result)
+}
+
+x=c(1,2,3,56,32,4,632,3,5.3)
+
+test.standard <- function() {
+  set.seed(88)
+  a = pboot(x, myfunction, 100)
+  set.seed(88)
+  b = boot(x, myfunction, 100)
+  a
+  b
+  checkEquals(a,b,"Test 0")
+}
+
+
