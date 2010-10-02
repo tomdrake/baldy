495b443b6427fa8d9cc74963b4f47dc34be8ce4c
laurence.baldwin <laurence.baldwin@gmail.com>
2010-10-02 20:06:43 +0000

    some refactoring added two functions Carray2Rmatrix that only works with int and Rmatrix2Carray that only works with doubles at the moment

--- sprint-0.2/src/algorithms/pboot/interface/pboot.c
+++ sprint-0.2/src/algorithms/pboot/interface/pboot.c
@@ -22,6 +22,8 @@
 #include "../../../sprint.h"
 #include "../../../functions.h"
 extern int boot(int n,...);
+void Carray2Rmatrix(double * array, SEXP matrix, int r, int c);
+void Rmatrix2Carray(SEXP matrix, int * array, int r, int c); 
 
 /* ******************************************************** *
  *  The stub for the R side of a very simple test command.  *
@@ -61,35 +63,47 @@ SEXP pboot(SEXP data, SEXP statistic, SEXP ind, SEXP lt0,... ){
     // convert the ind from (horrible) SEXP format to C array
     int * cind;
     cind = (int *)malloc(sizeof(int) * r * c);
-    int count = 0;
-    int cindx = 0; // position in the cind
-    for(i=0;i<r;i++){
-      cindx = i;
-      for(j=0;j<c;j++){
-        cind[count] = INTEGER(ind)[cindx];
-	count++;
-	cindx += r;
-      }
-    }
-    response = boot(1, func_results, translateChar(PRINTNAME(data)), translateChar(PRINTNAME(statistic)), ltn, r, c, cind);
-
-    PROTECT(result = allocMatrix(REALSXP,r ,ltn)); // t.star <- matrix(NA, sum(R), lt0)
+    Rmatrix2Carray(ind, cind, r, c);
     
- 
-    // jump through array to create SEXP in correct order
-    count = 0;
-    cindx = 0;
-    for(i=0; i < ltn; i++) {
-      cindx = i;
-      for (j=0; j <r; j++){
-        REAL(result)[count]= func_results[cindx];
-        count++;
-        cindx += ltn;
-      }
-    }
+    // sending everything to the implementation function
+    response = boot(1, func_results, translateChar(PRINTNAME(data)), translateChar(PRINTNAME(statistic)), ltn, r, c, cind);
+    free(cind);
+
+    // Turn the array passed back from the implementation into 
+    // a SEXP object that can be returned to R.
+    PROTECT(result = allocMatrix(REALSXP,r ,ltn)); // t.star <- matrix(NA, sum(R), lt0)
+    Carray2Rmatrix(func_results, result, r, ltn);
+    free(func_results);
     
     //PrintValue(result);
-    free(func_results);
     UNPROTECT(1); 
     return result;
+}
+
+void Rmatrix2Carray(SEXP matrix, int * array, int r, int c){
+  int i,j;
+  int count = 0;
+  int indx = 0; 
+  for(i=0;i<r;i++){
+    indx = i;
+    for(j=0;j<c;j++){
+      array[count] = INTEGER(matrix)[indx];
+      count++;
+      indx += r;
+    }
+  }
+}
+
+void Carray2Rmatrix(double * array, SEXP matrix, int r, int c){
+  int i, j; 
+  int count = 0;
+  int indx = 0;
+  for(i=0; i < c; i++) {
+    indx = i;
+    for (j=0; j <r; j++){
+      REAL(matrix)[count]= array[indx];
+      count++;
+      indx += c;
+    }
+  }
 }
